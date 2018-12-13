9792ddf8110d821251585fe42cf2eb206eb32516
Karl Xu <karl.yogi.xu@gmail.com>
2018-12-14 01:35:32 +0800

    output log in json

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 import logging
+import json_log_formatter
 
+formatter = json_log_formatter.JSONFormatter()
+json_handler = logging.StreamHandler()
+json_handler.setFormatter(formatter)
 logger = logging.getLogger(__name__)
+logger.addHandler(json_handler)
+
 
 def homepage(request):
     return HttpResponse('homepage')
