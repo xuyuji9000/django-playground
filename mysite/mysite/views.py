d638b5a3d5d6edfe742ca3ad0d58ea7458ca21a4
Karl Xu <karl.yogi.xu@gmail.com>
2018-12-14 01:18:49 +0800

    add critical log

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+import logging
+
+logger = logging.getLogger(__name__)
 
 def homepage(request):
     return HttpResponse('homepage')
+
+def critical_log(request):
+    logger.critical('Something went wrong!')
+    return HttpResponse('critical_log')
